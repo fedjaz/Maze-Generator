598d6572e1e045da01bd8a4d8e5480913a38ff39
Федор Юрецкий <fedjaz5@gmail.com>
2020-04-04 16:02:42 +0300

    added maze and cell class

---
+++ Maze Generator/Maze Generator/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Generator
+{
+    public class Cell
+    {
+        public bool Up { get; set; }
+        public bool Right { get; set; }
+        public bool Down { get; set; }
+        public bool Left { get; set; }
+
+        public Cell()
+        {
+            Up = false;
+            Right = false;
+            Down = false;
+            Left = false;
+        }
+    }
+}
---
+++ Maze Generator/Maze Generator/Maze.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Generator
+{
+    public class Maze
+    {
+        public int SizeX { get; }
+        public int SizeY { get; }
+        Cell[,] cells;
+
+        public Maze(int y, int x)
+        {
+            cells = new Cell[y, x];
+            for(int i = 0; i < y; i++)
+            {
+                for(int j = 0; j < x; j++)
+                {
+                    cells[i, j] = new Cell();
+                }
+            }
+            SizeX = x;
+            SizeY = y;
+        }
+
+        public Cell this[int i, int j]
+        {
+            get => cells[i, j];
+            set => cells[i, j] = value;
+        }
+    }
+}
