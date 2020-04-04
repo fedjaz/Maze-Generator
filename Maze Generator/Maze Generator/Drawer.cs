c303ad83547a5ad7ad4ae264956d7629f70024fc
Федор Юрецкий <fedjaz5@gmail.com>
2020-04-04 18:09:02 +0300

    added Drawer class

---
+++ Maze Generator/Maze Generator/Drawer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Generator
+{
+    class Drawer
+    {
+        public static Bitmap Draw(Maze maze, float maxResolution)
+        {
+            float cellSize = maxResolution / Math.Max(maze.SizeX, maze.SizeY);
+            Bitmap bitmap = new Bitmap((int)(maze.SizeX * cellSize), 
+                                       (int)(maze.SizeY * cellSize));
+            using(Graphics g = Graphics.FromImage(bitmap))
+            {
+                float wallWidth = cellSize * 0.15f;
+                g.FillRectangle(new SolidBrush(Color.White), 0, 0, 
+                                bitmap.Size.Width, 
+                                bitmap.Size.Height);
+                g.FillRectangle(new SolidBrush(Color.Green), 0, 0,
+                                cellSize - wallWidth / 2, 
+                                cellSize - wallWidth / 2);
+                g.FillRectangle(new SolidBrush(Color.Red),
+                                cellSize * (maze.SizeX - 1) + wallWidth / 2, 
+                                cellSize * (maze.SizeY - 1) + wallWidth / 2, 
+                                cellSize,
+                                cellSize);
+                Pen p = new Pen(Color.Black, wallWidth);
+                p.Width *= 2;
+                g.DrawRectangle(p, 0, 0, bitmap.Size.Width, bitmap.Size.Height);
+                p.Width /= 2;
+
+                for(int i = 0; i < maze.SizeY; i++)
+                {
+                    for(int j = 0; j < maze.SizeX; j++)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.Black),
+                                        j * cellSize - wallWidth / 2,
+                                        i * cellSize - wallWidth / 2,
+                                        wallWidth,
+                                        wallWidth);
+
+                        if(!maze[i, j].Down)
+                        {
+                            g.DrawLine(p, 
+                                       new PointF(j * cellSize, (i + 1) * cellSize),
+                                       new PointF((j + 1) * cellSize, (i + 1) * cellSize));
+                        }
+                        if(!maze[i, j].Right)
+                        {
+                            g.DrawLine(p, 
+                                       new PointF((j + 1) * cellSize, i * cellSize),
+                                       new PointF((j + 1) * cellSize, (i + 1) * cellSize));
+                        }
+                    }
+                }
+            }
+            return bitmap;
+        }
+
+        public static Bitmap DrawSolution(Maze maze, Bitmap bitmap)
+        {
+            float cellSize = bitmap.Width / maze.SizeX;
+            List<Maze.Directions> directions = maze.Solve();
+            List<PointF> points = new List<PointF>();
+            points.Add(new PointF(cellSize / 2, cellSize / 2));
+            foreach(Maze.Directions dir in directions)
+            {
+                if(dir == Maze.Directions.Up)
+                {
+                    points.Add(new PointF(points.Last().X, 
+                                          points.Last().Y - cellSize));
+                }
+                if(dir == Maze.Directions.Right)
+                {
+                    points.Add(new PointF(points.Last().X + cellSize, 
+                                          points.Last().Y));
+                }
+                if(dir == Maze.Directions.Down)
+                {
+                    points.Add(new PointF(points.Last().X, 
+                                          points.Last().Y + cellSize));
+                }
+                if(dir == Maze.Directions.Left)
+                {
+                    points.Add(new PointF(points.Last().X - cellSize, 
+                                          points.Last().Y));
+                }
+            }
+            using(Graphics g = Graphics.FromImage(bitmap))
+            {
+                float wallWidth = cellSize * 0.15f;
+                Pen p = new Pen(Color.Red, wallWidth);
+                g.DrawLines(p, points.ToArray());
+            }
+            return bitmap;
+        }
+    }
+}
--- Maze Generator/Maze Generator/Maze.cs
+++ Maze Generator/Maze Generator/Maze.cs
@@ -8,6 +8,13 @@ namespace Maze_Generator
 {
     public class Maze
     {
+        public enum Directions
+        {
+            Up,
+            Right,
+            Down,
+            Left
+        }
         public int SizeX { get; }
         public int SizeY { get; }
         Cell[,] cells;
@@ -30,6 +37,81 @@ namespace Maze_Generator
         {
             get => cells[i, j];
             set => cells[i, j] = value;
+        }
+
+        public List<Directions> Solve()
+        {
+            return Solve(this);
+        }
+
+        public static List<Directions> Solve(Maze maze)
+        {
+            List<Directions> ans = new List<Directions>();
+            int[,] solution = Bfs(maze);
+            int y = maze.SizeY - 1, x = maze.SizeX - 1;
+            while(!(x == 0 && y == 0))
+            {
+                Directions direction = Directions.Up;
+                if(maze[y, x].Up && solution[y - 1, x] < solution[y, x])
+                {
+                    direction = Directions.Down;
+                    y--;
+                }
+                else if(maze[y, x].Right && solution[y, x + 1] < solution[y, x])
+                {
+                    direction = Directions.Left;
+                    x++;
+                }
+                else if(maze[y, x].Down && solution[y + 1, x] < solution[y, x])
+                {
+                    direction = Directions.Up;
+                    y++;
+                }
+                else if(maze[y, x].Left && solution[y, x - 1] < solution[y, x])
+                {
+                    direction = Directions.Right;
+                    x--;
+                }
+                ans.Add(direction);
+            }
+            ans.Reverse();
+            return ans;
+        }
+
+        static int[,] Bfs(Maze maze)
+        {
+            int[,] solution = new int[maze.SizeY, maze.SizeX];
+            bool[,] used = new bool[maze.SizeY, maze.SizeX];
+            Queue<Tuple<int, int>> cells = new Queue<Tuple<int, int>>();
+            cells.Enqueue(new Tuple<int, int>(0, 0));
+            while(cells.Count > 0)
+            {
+                Tuple<int, int> cell = cells.Dequeue();
+                int y = cell.Item1, x = cell.Item2;
+                used[y, x] = true;
+                if(maze[y, x].Down && !used[y + 1, x])
+                {
+                    solution[y + 1, x] = solution[y, x] + 1;
+                    cells.Enqueue(new Tuple<int, int>(y + 1, x));
+                }
+                if(maze[y, x].Up && !used[y - 1, x])
+                {
+                    solution[y - 1, x] = solution[y, x] + 1;
+                    cells.Enqueue(new Tuple<int, int>(y - 1, x));
+                }
+                if(maze[y, x].Left && !used[y, x - 1])
+                {
+                    solution[y, x - 1] = solution[y, x] + 1;
+                    cells.Enqueue(new Tuple<int, int>(y, x - 1));
+                }
+                if(maze[y, x].Right && !used[y, x + 1])
+                {
+                    solution[y, x + 1] = solution[y, x] + 1;
+                    cells.Enqueue(new Tuple<int, int>(y, x + 1));
+                }
+
+            }
+            return solution;
         }
     }
 }
